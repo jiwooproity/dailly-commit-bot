b86f7619e2b12a06052e4cc404bc6ed50d6b79bd
sojiwoo <58384366+jiwooproity@users.noreply.github.com>
2023-06-26 15:40:55 +0900

    feature: 어드민 전용 공지 명령어 권한 설정
    해당 명령어는 서버의 소유자이거나 길드에서만 사용이 가능한 명령어로 처리

--- apis/Fetch.js
+++ apis/Fetch.js
@@ -1,22 +1,29 @@
 const { Client, GatewayIntentBits, Partials } = require("discord.js");
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.DirectMessageTyping,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
   partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 });
 
 const Fetch = {
-    async getFetchGuildMemberIds({ guildId }) {
-        const guilds = await client.guilds.fetch(guildId);
-        const members = await guilds.members.fetch();
-        const users = members.filter((m) => m.user.discriminator !== '2140');
-        return users.map((user) => user.id);
-    },
-    async getFetchUsers({ userList }) {
-        return await Promise.all(userList.map((id) => client.users.fetch(id)));
-    },
-    async getFetchUser({ userId }) {
-        return await client.users.fetch(userId);
-    }
+  async getFetchGuildMemberIds({ guildId }) {
+    const guilds = await client.guilds.fetch(guildId);
+    const members = await guilds.members.fetch();
+    const users = members.filter((m) => m.user.discriminator !== "2140");
+    return users.map((user) => user.id);
+  },
+  async getFetchUsers({ userList }) {
+    return await Promise.all(userList.map((id) => client.users.fetch(id)));
+  },
+  async getFetchUser({ userId }) {
+    return await client.users.fetch(userId);
+  },
 };
 
 client.login(process.env.DISCORD_TOKEN);
