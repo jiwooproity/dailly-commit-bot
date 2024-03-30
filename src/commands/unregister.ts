982c1fa3a70212b8190b1fd62b37abb12fdbc777
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-03-31 03:59:43 +0900

    Fix: 가입/탈퇴 문구 수정

--- src/commands/unregister.ts
+++ src/commands/unregister.ts
@@ -1,4 +1,4 @@
-import { ApplicationCommandOptionType } from "discord.js";
+import { ApplicationCommandOptionType, EmbedBuilder } from "discord.js";
 
 import { Command } from "../types";
 import { deleteUser } from "../firebase";
@@ -15,13 +15,17 @@ export const unregister: Command = {
     },
   ],
   execute: async (_, interaction) => {
+    const embed = new EmbedBuilder();
+
     try {
       await deleteUser(interaction);
-      const msg = "탈퇴가 무사히 완료되었습니다. 또 이용해 주세요!";
-      interaction.followUp({ content: msg });
+      embed.setTitle("탈퇴가 무사히 완료되었습니다.");
+      embed.setDescription("그동안 고생 많으셨습니다. 항상 응원하겠습니다.");
+      interaction.followUp({ embeds: [embed] });
     } catch (e) {
-      const msg = "이미 탈퇴하셨거나, 요청에 실패하였습니다. 다시 시도해 주세요!";
-      interaction.followUp({ content: msg });
+      embed.setTitle("이미 탈퇴하셨거나, 요청에 실패하였습니다.");
+      embed.setDescription("일시적인 오류일 수도 있습니다. 다시 시도해 주세요.");
+      interaction.followUp({ embeds: [embed] });
     }
   },
 };
